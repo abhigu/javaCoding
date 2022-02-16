1208ce64eeb2cf3473245b75c10390db1b1677fb
abhigu <93052364+abhigu@users.noreply.github.com>
2022-02-15 21:50:52 -0800

    Now with lambdas!

--- workspace 2/java/src/Viruses/Node.java
+++ workspace 2/java/src/Viruses/Node.java
@@ -1,7 +1,39 @@
 package Viruses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
 	Varient varient;
+	List<Varient> immunities = new ArrayList<Varient>(); 
+	
+	boolean living; 
+	
 	int id;
 	int density;
+	
+	int dayOfInfection;
+	int dayOfHeal;
+			
+	public void infect(Varient varient, int day) {
+		this.varient = varient;
+		this.dayOfInfection = day;
+		this.dayOfHeal = dayOfInfection + (int) varient.incubation;
+	}
+	
+	public void checkHealed(int day) {
+		if(!!living && day >= this.dayOfHeal) {
+			this.immunities.add(this.varient);
+			varient = null;
+		}
+	}
+	
+	public boolean isImmune(Varient varient) {
+		for(Varient immune : immunities) {
+			if(varient == immune) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
